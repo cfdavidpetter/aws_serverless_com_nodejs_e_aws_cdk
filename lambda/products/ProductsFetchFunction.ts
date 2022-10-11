ee4477d6816a94275bddce7525750887c1a8720f
David Petter Alves <david.petter@condoconta.com.br>
2022-10-11 19:25:19 -0300

    first commit

---
+++ lambda/products/ProductsFetchFunction.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const method = event.httpMethod
+    const lambdaRequestId = context.awsRequestId
+    const apiRequestId = event.requestContext.requestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+    
+
+    if (event.resource === "/products") {
+        if (method === 'GET') {
+            console.log('GET');
+            
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "GET Products - OK"
+                })
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: "Bad request"
+        })
+    }
+}
